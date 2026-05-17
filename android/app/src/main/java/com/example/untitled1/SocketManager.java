ef9cb1e5ceb60576e198fa100b099739697c7f35
me5927946-debug <me5927946@gmail.com>
2026-05-17 14:53:50 +0300

    Update SocketManager.java

--- android/app/src/main/java/com/example/untitled1/SocketManager.java
+++ android/app/src/main/java/com/example/untitled1/SocketManager.java
@@ -1,5 +1,9 @@
 package com.example.untitled1;
 
+import android.util.Log;
+
+import java.net.URISyntaxException;
+
 import io.socket.client.IO;
 import io.socket.client.Socket;
 
@@ -15,22 +19,50 @@ public class SocketManager {
 
                 IO.Options options = new IO.Options();
 
-                // IMPORTANT
-                options.forceNew = true;
-                options.reconnection = true;
+                options.transports =
+                        new String[]{"websocket", "polling"};
 
-                // USE BOTH
-                options.transports = new String[] {
-                        "websocket",
-                        "polling"
-                };
+                options.reconnection = true;
+                options.forceNew = false;
+                options.timeout = 20000;
 
                 socket = IO.socket(
-                        "http://192.168.1.7:8000",
+                        "http://192.168.1.64:8000",
                         options
                 );
 
-            } catch (Exception e) {
+                socket.on(Socket.EVENT_CONNECT, args -> {
+                    Log.d("SOCKET", "CONNECTED SUCCESSFULLY");
+                });
+
+                socket.on(Socket.EVENT_DISCONNECT, args -> {
+                    Log.e("SOCKET", "DISCONNECTED");
+                });
+
+                socket.on(Socket.EVENT_CONNECT_ERROR, args -> {
+
+                    Log.e("SOCKET", "CONNECT ERROR");
+
+                    if (args.length > 0) {
+                        Log.e(
+                                "SOCKET",
+                                args[0].toString()
+                        );
+                    }
+                });
+
+                socket.on("new_alert", args -> {
+
+                    Log.d(
+                            "SOCKET",
+                            "NEW ALERT RECEIVED"
+                    );
+                });
+
+                socket.connect();
+
+            } catch (URISyntaxException e) {
+
                 e.printStackTrace();
             }
         }
