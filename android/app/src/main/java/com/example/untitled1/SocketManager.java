374f3649f1432d4ef846dab125d6f504ca1c6d8f
me5927946-debug <me5927946@gmail.com>
2026-05-19 02:12:35 +0300

    Update SocketManager.java

--- android/app/src/main/java/com/example/untitled1/SocketManager.java
+++ android/app/src/main/java/com/example/untitled1/SocketManager.java
@@ -23,8 +23,6 @@ public class SocketManager {
                         new String[]{"websocket", "polling"};
 
                 options.reconnection = true;
-                options.forceNew = false;
-                options.timeout = 20000;
 
                 socket = IO.socket(
                         "http://192.168.1.64:8000",
@@ -32,31 +30,33 @@ public class SocketManager {
                 );
 
                 socket.on(Socket.EVENT_CONNECT, args -> {
-                    Log.d("SOCKET", "CONNECTED SUCCESSFULLY");
-                });
 
-                socket.on(Socket.EVENT_DISCONNECT, args -> {
-                    Log.e("SOCKET", "DISCONNECTED");
+                    Log.d(
+                            "SOCKET",
+                            "CONNECTED SUCCESSFULLY"
+                    );
+
+                    // REGISTER DRIVER
+                    socket.emit(
+                            "register_driver",
+                            1
+                    );
                 });
 
                 socket.on(Socket.EVENT_CONNECT_ERROR, args -> {
 
-                    Log.e("SOCKET", "CONNECT ERROR");
+                    Log.e(
+                            "SOCKET",
+                            "CONNECT ERROR"
+                    );
 
                     if (args.length > 0) {
+
                         Log.e(
                                 "SOCKET",
                                 args[0].toString()
                         );
                     }
-                });
-
-                socket.on("new_alert", args -> {
-
-                    Log.d(
-                            "SOCKET",
-                            "NEW ALERT RECEIVED"
-                    );
                 });
 
                 socket.connect();
