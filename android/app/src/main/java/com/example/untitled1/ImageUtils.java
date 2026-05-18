1780c6ad94db10a1e106f5315599d36dea03ae62
me5927946-debug <me5927946@gmail.com>
2026-05-19 02:13:35 +0300

    Update ImageUtils.java

--- android/app/src/main/java/com/example/untitled1/ImageUtils.java
+++ android/app/src/main/java/com/example/untitled1/ImageUtils.java
@@ -1,41 +1,44 @@
 package com.example.untitled1;
 
-import android.media.Image;
 import androidx.camera.core.ImageProxy;
 
 import java.nio.ByteBuffer;
 
 public class ImageUtils {
 
-    // REQUIRED METHOD (matches your CameraActivity call)
-    public static byte[] toByteArray(ImageProxy image) {
+    public static byte[] toByteArray(
+            ImageProxy image
+    ) {
 
-        ImageProxy.PlaneProxy[] planes = image.getPlanes();
+        ByteBuffer yBuffer =
+                image.getPlanes()[0].getBuffer();
 
-        ByteBuffer yBuffer = planes[0].getBuffer();
-        ByteBuffer uBuffer = planes[1].getBuffer();
-        ByteBuffer vBuffer = planes[2].getBuffer();
+        ByteBuffer uBuffer =
+                image.getPlanes()[1].getBuffer();
+
+        ByteBuffer vBuffer =
+                image.getPlanes()[2].getBuffer();
 
         int ySize = yBuffer.remaining();
         int uSize = uBuffer.remaining();
         int vSize = vBuffer.remaining();
 
-        byte[] nv21 = new byte[ySize + uSize + vSize];
+        byte[] nv21 =
+                new byte[ySize + uSize + vSize];
 
         yBuffer.get(nv21, 0, ySize);
 
-        byte[] uBytes = new byte[uSize];
-        byte[] vBytes = new byte[vSize];
+        vBuffer.get(
+                nv21,
+                ySize,
+                vSize
+        );
 
-        uBuffer.get(uBytes);
-        vBuffer.get(vBytes);
-
-        int pos = ySize;
-
-        for (int i = 0; i < uSize; i++) {
-            nv21[pos++] = vBytes[i];
-            nv21[pos++] = uBytes[i];
-        }
+        uBuffer.get(
+                nv21,
+                ySize + vSize,
+                uSize
+        );
 
         return nv21;
     }
