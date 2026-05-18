fbbdc70835fd3d7bc72747fca2989e1dd20a351e
me5927946-debug <me5927946@gmail.com>
2026-05-19 02:14:17 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+
 import 'package:untitled1/profile_screen.dart';
 import 'package:untitled1/signup_screen.dart';
-import 'login_screen.dart';
+import 'package:untitled1/login_screen.dart';
+
 import 'services/socket_service.dart';
+
 // Screens
-import 'Splash_Screen.dart'; // WelcomeScreen
+import 'Splash_Screen.dart';
 import 'history_screen.dart';
 import 'home_screen.dart';
 import 'camera_screen.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  SocketService.connect(); // 🔥 START REAL-TIME CONNECTION
+
+  // START SOCKET SERVICE
+  SocketService.connect();
+
   runApp(const MyApp());
-
-
-
 }
 
-// 🔥 Changed to StatefulWidget
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // 👉 Function to change language from anywhere
   static void setLocale(BuildContext context, Locale locale) {
     final _MyAppState? state =
     context.findAncestorStateOfType<_MyAppState>();
+
     state?.setLocale(locale);
   }
 
@@ -35,9 +37,9 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+
   Locale? _locale;
 
-  // 👉 Update locale
   void setLocale(Locale locale) {
     setState(() {
       _locale = locale;
@@ -46,34 +48,46 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
+
       debugShowCheckedModeBanner: false,
+
       title: 'Alerto',
 
-      // 🌍 Localization
+      // LOCALIZATION
       supportedLocales: const [
         Locale('en'),
         Locale('ar'),
       ],
+
       localizationsDelegates: const [
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
         GlobalCupertinoLocalizations.delegate,
       ],
 
-      // 🔥 Dynamic language
       locale: _locale,
 
-      // 🧭 Routes
+      // ROUTES
       initialRoute: '/',
+
       routes: {
+
         '/': (context) => const WelcomeScreen(),
+
         '/home': (context) => const HomeScreen(),
+
         '/camera': (context) => const CameraScreen(),
+
         '/profile': (context) => const ProfileScreen(),
+
         '/signup': (context) => const SignUpScreen(),
+
         '/login': (context) => const LoginScreen(),
+
         '/history': (context) => const HistoryScreen(),
+
         '/splash': (context) => const WelcomeScreen(),
       },
     );
